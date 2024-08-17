5586e050674550cffed33b707e5dfa85d047896a
manikanta lokesh <manikantalokesh7@gmail.com>
2024-08-17 22:49:10 +0530

    first commit

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,10 @@
+// import { Link } from "react-router-dom";
+// import Header from "../Header";
+function IndexPage(){
+    return (
+    <div>
+      index page here
+    </div>
+    );
+}
+export default IndexPage;
